5e168abba16080041649e3c7016648ef614cd0ed
Luc Gouzy <lg-1@live.fr>
2023-07-05 13:19:55 +0200

    started home page, banner
    made some test to understand how to use react properly

---
+++ p8-lucg-reactapp/src/components/banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../assets/images/logo.png';
+import '../assets/styles/main.scss'
+
+function Banner (){
+    return(<div className="banner">
+        <img className="bannerlogo" alt="logo kansas" src={logo}/>
+        <div>
+            <a href="#" className="bannerlink">Acceuil</a>
+            <a href="#" className="bannerlink">A Propos</a>
+        </div>
+    </div>
+
+    )
+}
+
+export default Banner;
