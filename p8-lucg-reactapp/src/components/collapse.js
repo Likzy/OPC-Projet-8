8d8b476a8e2910866c3bf18d86b49469b645dcaa
Luc Gouzy <lg-1@live.fr>
2023-07-12 13:13:16 +0200

    about page is now functionnal

--- p8-lucg-reactapp/src/components/collapse.js
+++ p8-lucg-reactapp/src/components/collapse.js
@@ -2,14 +2,23 @@ import React from 'react';
 import arrow from '../assets/images/arrow-back-ios-24-px-2.png';
 
 function Collapse({title,content}){
+    
+    const [isDescriptionOpen, setIsDescriptionOpen] = React.useState(false);
+    
+    const handleClick = () => {
+        setIsDescriptionOpen(!isDescriptionOpen);
+        console.log('lol')
+    };
+    
     return(<div className='collapseparent'>
         <div className='collapse'>
         <p className='collapsetitle'>{title}</p>
-        <img className="arrowimage" src={arrow} alt='Arrow to open and close'></img>
+        <img className={`arrow ${isDescriptionOpen ? 'open' : ''} arrowimage`} src={arrow} alt='Arrow to open and close' onClick={handleClick}></img>
         </div>
-        <div className='collapsecontent'>
-            <p>{content}</p>
+        <div className={`description ${isDescriptionOpen ? 'open' : ''} collapsecontent`}>
+        <p>{content}</p> 
         </div>
+        
 
     </div>
     )
