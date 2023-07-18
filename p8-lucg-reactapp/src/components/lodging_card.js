8d02a2a3597039d32a5f9c971a80f79393a4fade
Luc Gouzy <lg-1@live.fr>
2023-07-18 23:34:06 +0200

    fixed grid for home page, can start working on responsive

--- p8-lucg-reactapp/src/components/lodging_card.js
+++ p8-lucg-reactapp/src/components/lodging_card.js
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 
-function LodgingCard(props){
-    const logement = props.logement
-    return (
-        <div className="lodgingcard">
-          <Link to={`/logement/${logement.id}`} >
-            <img className="lodgingcover" src={logement.cover} alt={logement.title} />
-            <div className="shadowcover"></div>
-            <p className="lodgingtitle">{logement.title}</p>
-          </Link>
-        </div>
-      );
-};
-
+function LodgingCard(props) {
+  const logement = props.logement;
+  return (
+    <div className="lodgingcard">
+      <Link to={`/logement/${logement.id}`}>
+        <img
+          className="lodgingcover"
+          src={logement.cover}
+          alt={logement.title}
+        />
+        <div className="shadowcover"></div>
+        <p className="lodgingtitle">{logement.title}</p>
+      </Link>
+    </div>
+  );
+}
 
 export default LodgingCard;
